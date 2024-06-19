e8d39f5b2ee845e0e0d00cd6c822d0d4d243b414
Jay Kim <jbkim3510@gmail.com>
2024-06-19 14:52:06 -0700

    End of section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/Store'
 import { RouterProvider } from 'react-router-dom'
