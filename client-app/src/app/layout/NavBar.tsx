fda230690ba7d9b2d402d88b6e6cb75d3f1b3a56
Jay Kim <jbkim3510@gmail.com>
2024-06-18 21:30:11 -0700

    End of section 8

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,19 @@
-
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/Store';
+import { NavLink } from 'react-router-dom';
 
 
 export default function NavBar() {
 
-    const {activityStore} = useStore();
     return(
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name ='activities'/>
-                <Menu.Item> 
-                    <Button onClick={()=> activityStore.openForm()} positive content='Create Activity'/>
+                <Menu.Item as={NavLink} to='/activities' name ='activities'/>
+                <Menu.Item > 
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
