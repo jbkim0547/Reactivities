c6361b049f1877c4e1d1152274b65a8722c69679
Jay Kim <jbkim3510@gmail.com>
2024-10-21 20:59:43 -0700

    End of section 19

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -2,6 +2,7 @@ using Domain;
 using AutoMapper;
 using Application.Activities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Application.Comments;
 
 namespace Application.Core
 {
@@ -11,15 +12,20 @@ namespace Application.Core
         {
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
-                .ForMember(d=> d.HostUsername, o=> o.MapFrom(s=> s.Attendees
-                    .FirstOrDefault(x=> x.IsHost).AppUser.UserName));
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
+                    .FirstOrDefault(x => x.IsHost).AppUser.UserName));
             CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
-                .ForMember(d=> d.Image, o=> o.MapFrom(s=> s.AppUser.Photos.FirstOrDefault(x=> x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d=> d.Image, o=> o.MapFrom(s=> s.Photos.FirstOrDefault(x=> x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Comment, CommentDto>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
+
         }
     }
 }
