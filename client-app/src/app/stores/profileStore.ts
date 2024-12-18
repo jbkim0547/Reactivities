63d94cbc45875f4756ddb7d83b5d0e2697f846b8
Jay Kim <jbkim3510@gmail.com>
2024-12-17 18:59:16 -0800

    End of section 21

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable, reaction, runInAction } from "mobx";
-import { Photo, Profile } from "../models/profile";
+import { Photo, Profile, UserActivity } from "../models/profile";
 import agent from "../api/agent";
 import { store } from "./store";
 
@@ -11,14 +11,16 @@ export default class ProfileStore {
     followings: Profile[] = [];
     loadingFollowings = false;
     activeTab = 0;
+    userActivities: UserActivity[] = [];
+    loadingActivities = false;
 
     constructor() {
         makeAutoObservable(this);
 
         reaction(
-            ()=> this.activeTab,
-            activeTab =>{
-                if(activeTab === 3 || activeTab === 4){
+            () => this.activeTab,
+            activeTab => {
+                if (activeTab === 3 || activeTab === 4) {
                     const predicate = activeTab === 3 ? 'followers' : 'following';
                     this.loadFollowings(predicate);
                 } else {
@@ -134,39 +136,55 @@ export default class ProfileStore {
         try {
             await agent.Profiles.updateFollowing(username);
             store.activityStore.updateAttendeeFollowing(username);
-            runInAction(()=>{
-                if(this.profile && this.profile.username !== store.userStore.user?.username && this.profile.username === username){
+            runInAction(() => {
+                if (this.profile && this.profile.username !== store.userStore.user?.username && this.profile.username === username) {
                     following ? this.profile.followersCount++ : this.profile.followersCount--;
                     this.profile.following = !this.profile.following;
                 }
-                if(this.profile && this.profile.username === store.userStore.user?.username){
+                if (this.profile && this.profile.username === store.userStore.user?.username) {
                     following ? this.profile.followersCount++ : this.profile.followersCount--;
                 }
                 this.followings.forEach(profile => {
                     if (profile.username === username) {
-                        profile.following ? profile.followersCount--: profile.followersCount++;
+                        profile.following ? profile.followersCount-- : profile.followersCount++;
                         profile.following = !profile.following;
                     }
                 })
                 this.loading = false;
             })
-        } catch(error){
+        } catch (error) {
             console.log(error);
-            runInAction(()=> this.loading = false);
+            runInAction(() => this.loading = false);
         }
     }
 
-    loadFollowings = async (predicate: string) =>{
+    loadFollowings = async (predicate: string) => {
         this.loadingFollowings = true;
-        try{
+        try {
             const followings = await agent.Profiles.listFollowings(this.profile!.username, predicate)
-            runInAction(()=>{
+            runInAction(() => {
                 this.followings = followings;
                 this.loadingFollowings = false;
             })
-        } catch(error){
+        } catch (error) {
             console.log(error);
-            runInAction(()=> this.loadingFollowings = false);
+            runInAction(() => this.loadingFollowings = false);
+        }
+    }
+
+    loadUserActivities = async (username: string, predicate?: string) => {
+        this.loadingActivities = true;
+        try {
+            const activities = await agent.Profiles.listActivities(username, predicate!);
+            runInAction(() => {
+                this.userActivities = activities;
+                this.loadingActivities = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loadingActivities = false;
+            })
         }
     }
 }
--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -6,27 +6,28 @@ import { Profile } from "../../app/models/profile";
 import ProfileAbout from "./ProfileAbout";
 import ProfileFollowings from "./ProfileFollowings";
 import { useStore } from "../../app/stores/store";
+import ProfileActivities from "./ProfileActivities";
 
 interface Props {
     profile: Profile;
 }
 
-export default observer (function ProfileContent({profile}: Props){
-    const {profileStore} = useStore();
+export default observer(function ProfileContent({ profile }: Props) {
+    const { profileStore } = useStore();
     const panes = [
-        {menuItem: 'About', render:()=> <ProfileAbout />},
-        {menuItem: 'Photos', render:()=> <ProfilePhotos  profile={profile}/>},
-        {menuItem: 'Events', render:()=> <TabPane>Events Content</TabPane>},
-        {menuItem: 'Followers', render:()=> <ProfileFollowings/>},
-        {menuItem: 'Following', render:()=> <ProfileFollowings/>}
+        { menuItem: 'About', render: () => <ProfileAbout /> },
+        { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
+        { menuItem: 'Events', render: () => <ProfileActivities /> },
+        { menuItem: 'Followers', render: () => <ProfileFollowings /> },
+        { menuItem: 'Following', render: () => <ProfileFollowings /> }
     ];
 
     return (
         <Tab
-            menu={{fluid: true, vertical: true}}
+            menu={{ fluid: true, vertical: true }}
             menuPosition="right"
-            panes={panes}    
-            onTabChange={(_, data)=> profileStore.setActiveTab(data.activeIndex as number)}
+            panes={panes}
+            onTabChange={(_, data) => profileStore.setActiveTab(data.activeIndex as number)}
         />
     )
 })
