b756c505bc40c5a8d5a75288eb4301b0c522212b
Jay Kim <jbkim3510@gmail.com>
2025-01-25 16:50:24 -0800

    update for CI

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,7 +5,7 @@ import 'react-calendar/dist/Calendar.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 import 'react-datepicker/dist/react-datepicker.css'
 import './app/layout/styles.css'
-import { StoreContext, store } from './app/stores/store';
+import { StoreContext, store } from './app/stores/store'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes'
 
